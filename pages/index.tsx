b82ab4cbb5fd7f2a9a240c4af12ecdc562279b41
matt <mattsnaccc@gmail.com>
2021-07-10 00:55:37 -0700

    fix: seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import {Page, Text, Snippet, Grid, Spacer} from "@geist-ui/react";
+import {NextSeo} from "next-seo";
 import React from "react";
+import Head from 'next/head';
+
 import {Footer} from "../lib/Footer";
 
 export default function Home() {
@@ -13,65 +16,95 @@ export default function Home() {
   });
 
   return (
-      <Page dotBackdrop size="large">
-        <Grid.Container justify="center" alignItems="center" direction="column">
-          <Grid>
-            <Text h1 style={{ position: "relative" }}>
-              presence
-              <span
-                  style={{
-                    position: "absolute",
-                    top: "-0.75rem",
-                    right: "-0.95rem",
-                  }}
-              >
-              {hours >= 17 || hours <= 6 ? (
-                  <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="#fee75c"
+      <>
+          <Head>
+              <link rel="icon" href="/favicon.ico" />
+              <meta name="theme-color" content="#2f3136" />
+          </Head>
+          <NextSeo
+              title="react.presence"
+              description="An official react hook for presence."
+              openGraph={{
+                  type: "website",
+                  url: "https://react.presence.im",
+                  title: "react.presence",
+                  description:
+                      "An official react hook for presence.",
+                  images: [
+                      {
+                          url: "https://presence.im/presence.png",
+                          width: 400,
+                          height: 200,
+                      },
+                  ],
+              }}
+              twitter={{
+                  handle: "@atmattt",
+                  site: "@atmattt",
+                  cardType: "summary_large_image",
+              }}
+          />
+
+          <Page dotBackdrop size="large">
+            <Grid.Container justify="center" alignItems="center" direction="column">
+              <Grid>
+                <Text h1 style={{ position: "relative" }}>
+                  presence
+                  <span
                       style={{
-                        height: "1.25rem",
-                        width: "1.25rem",
-                        backgroundColor: "white",
-                        borderRadius: "10rem",
+                        position: "absolute",
+                        top: "-0.75rem",
+                        right: "-0.95rem",
                       }}
                   >
-                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-                  </svg>
-              ) : (
-                  <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="#fee75c"
-                      style={{
-                        height: "1.25rem",
-                        width: "1.25rem",
-                        backgroundColor: "white",
-                        borderRadius: "10rem",
-                      }}
-                  >
-                    <path
-                        fillRule="evenodd"
-                        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                        clipRule="evenodd"
-                    />
-                  </svg>
-              )}
-            </span>
-            </Text>
-          </Grid>
-          <Grid>
-            <p>An official react hook for presence.</p>
-          </Grid>
-          <Grid>
-            <Spacer y={2}/>
-          </Grid>
-          <Grid>
-              <Snippet text="yarn add use-presence" width="300px" />
-          </Grid>
-          <Footer />
-        </Grid.Container>
-      </Page>
+                  {hours >= 17 || hours <= 6 ? (
+                      <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="#fee75c"
+                          style={{
+                            height: "1.25rem",
+                            width: "1.25rem",
+                            backgroundColor: "white",
+                            borderRadius: "10rem",
+                          }}
+                      >
+                        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+                      </svg>
+                  ) : (
+                      <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="#fee75c"
+                          style={{
+                            height: "1.25rem",
+                            width: "1.25rem",
+                            backgroundColor: "white",
+                            borderRadius: "10rem",
+                          }}
+                      >
+                        <path
+                            fillRule="evenodd"
+                            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                            clipRule="evenodd"
+                        />
+                      </svg>
+                  )}
+                </span>
+                </Text>
+              </Grid>
+              <Grid>
+                <p>An official react hook for presence.</p>
+              </Grid>
+              <Grid>
+                <Spacer y={2}/>
+              </Grid>
+              <Grid>
+                  <Snippet text="yarn add use-presence" width="300px" />
+              </Grid>
+              <Footer />
+            </Grid.Container>
+          </Page>
+      </>
   )
 }
